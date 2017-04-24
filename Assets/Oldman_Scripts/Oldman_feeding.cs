cf106c2eb993f38cc024c6540082c267305abb5e
KobeyMyhre <kobey.myhre@students.aie.edu.au>
2017-04-24 11:45:43 -0700

    MassDelete

--- Assets/Oldman_Scripts/Oldman_feeding.cs
+++ Assets/Oldman_Scripts/Oldman_feeding.cs
@@ -7,14 +7,15 @@ public class Oldman_feeding : MonoBehaviour
     public GameObject SpawnFood;
     public float radius;
     public float spawnCount;
-    
+    public GameObject Body;
     private float StartTime;
     private float StartCount;
     public GameObject[] Feeder;
     Vector3 kobey = Vector3.zero;
-
+    //Animator Anim;
     public void Start()
     {
+        //Anim = Body.GetComponent<Animator>();
         StartCount = spawnCount;
     }
     public void DoFeed()
@@ -28,25 +29,25 @@ public class Oldman_feeding : MonoBehaviour
         {
             float X = Random.Range(-radius, radius);
             float Z = Random.Range(-radius, radius);
-            SpawnFood.transform.position = new Vector3(kobey.x + X, kobey.y, kobey.z + Z);
+            SpawnFood.transform.position = new Vector3(kobey.x + X, kobey.y +2, kobey.z + Z);
             Instantiate(SpawnFood);
         }
     }
-    void OnCollisionEnter(Collision collider)
-    {
-        if (collider.gameObject.tag == "feed_plat")
-        {
-            kobey = transform.forward * 10;
+    //void OnCollisionEnter(Collision collider)
+    //{
+    //    if (collider.gameObject.tag == "feed_plat")
+    //    {
+    //        kobey = transform.forward * 10;
 
-            spawnCount = StartCount;
+    //        spawnCount = StartCount;
 
-            for (int i = 0; i < spawnCount; ++i)
-            {
-                float X = Random.Range(-radius, radius);
-                float Z = Random.Range(-radius, radius);
-                SpawnFood.transform.position = new Vector3(kobey.x + X, kobey.y, kobey.z + Z);
-                Instantiate(SpawnFood);
-            }
-        }
-    }
+    //        for (int i = 0; i < spawnCount; ++i)
+    //        {
+    //            float X = Random.Range(-radius, radius);
+    //            float Z = Random.Range(-radius, radius);
+    //            SpawnFood.transform.position = new Vector3(kobey.x + X, kobey.y, kobey.z + Z);
+    //            Instantiate(SpawnFood);
+    //        }
+    //    }
+    //}
 }
